5ebbdf193db8f67f34d44776ca89a97bca69694c
Gunjan Vyas <gunjan.vyas@moneyview.in>
2024-02-10 20:45:23 +0530

    made responsive

--- Works/script.js
+++ Works/script.js
@@ -37,6 +37,11 @@ function loading() {
 
 loading();
 
+// Function to scroll to a section
+function scrollToSection(sectionId) {
+  scroll.scrollTo(document.querySelector(sectionId));
+}
+
 const loco = () => {
   const scroll = new LocomotiveScroll({
     el: document.querySelector("[data-scroll-container]"),
@@ -45,6 +50,7 @@ const loco = () => {
 
   document.querySelector("#backToTop").addEventListener("click", () => {
     scroll.scrollTo(0);
+    // scroll.scrollTo(document.querySelector("#page2"));
   });
   var page2 = document.querySelector("#page2");
   var elems = document.querySelectorAll(".elem");
@@ -54,14 +60,13 @@ const loco = () => {
       var bgimg = elem.getAttribute("data-img"); //getting custome attributes
       console.log(bgimg);
       page2.style.backgroundImage = `url(${bgimg})`;
-      // page2.style.background = "red";
     });
   });
 
   // changing nav color when page2 is inview, using locomotive js
   scroll.on("scroll", (value, way, obj) => {
-    console.log("value", value, "way", way, "obj", obj);
-    if (value.scroll.y >= 880 && value.scroll.y <= 1760) changeColorTo("#ffffff");
+    // console.log("value", value, "way", way, "obj", obj);
+    if (value.scroll.y >= 788 && value.scroll.y <= 1576) changeColorTo("#ffffff");
     else changeColorTo("#000000");
   });
 
@@ -90,3 +95,21 @@ loco();
 
 // scrollToTriggerNavColorChange();
 
+
+// using viewport to check exact coordinates of a section using in value.scroll.y
+// function isInViewport(element) {
+//   console.log("chala");
+//   var rect = element.getBoundingClientRect();
+//   var html = document.documentElement;
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <= (window.innerHeight || html.clientHeight) &&
+//     rect.right <= (window.innerWidth || html.clientWidth)
+//   );
+// }
+
+// window.addEventListener("scroll", () => {
+//   console.log("chalo");
+//   console.log("wow", isInViewport(document.querySelector("#page2")));
+// });
